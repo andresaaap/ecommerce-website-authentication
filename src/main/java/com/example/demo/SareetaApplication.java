8b98169f590679326dd62ca170a237f9e336830a
andresaaap <andresaaap@hotmail.com>
2023-07-20 17:54:13 -0500

    feat: Configure app for tomcat deployment

--- src/main/java/com/example/demo/SareetaApplication.java
+++ src/main/java/com/example/demo/SareetaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -21,6 +22,11 @@ public class SareetaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SareetaApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SareetaApplication.class);
 	}
 
 }
